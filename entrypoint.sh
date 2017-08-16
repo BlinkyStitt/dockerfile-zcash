f59be6d61cb1aa80a6e2bb8198d154b3cd36de7c
Bryan Stitt <bryan@stitthappens.com>
2017-08-16 16:22:39 -0700

    fix and add tor to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # setup config, fetch params, and start the zcash daemon
 #
 # this runs from inside the container. don't run this on the host
-
-
-#!/bin/bash
 set -e
 
 # this if will check if the first argument is a flag
@@ -21,7 +18,27 @@ if [ "$1" = 'zcashd' ]; then
     # we don't just add --config to the exec because we always forget that when attaching
     cp /config/zcash.conf ~/.zcash/
 
-    # TODO: customize the config with -proxy or other things that can't handle DNS
+    # customize the config with -proxy or other things that can't handle DNS
+    # zcashd does not support name resolution for the proxy setting so we get the IP for the Tor container
+    if [ -n "$TOR_HOSTNAME" ]; then
+        until [ -n "$TOR_IP" ]; do
+            TOR_IP=$(getent hosts "$TOR_HOSTNAME" | cut -f1 -d' ')
+
+            # TODO: max wait?
+            if [ -z "$TOR_IP" ]; then
+                echo "Tor is not yet running at $TOR_HOSTNAME! Sleeping..."
+                sleep 10
+            fi
+        done
+
+        # proxy or onion
+        [ -z "$TOR_OPTION" ] && TOR_OPTION="proxy"
+
+        # TODO: allow onion option instead of proxy
+        echo "${TOR_OPTION}=${TOR_IP}:9050" >> ~/.zcash/zcash.conf
+
+        # TODO: get the hidden service address
+    fi
 
     # TODO: lock to make sure multiple containers aren't fetching at the same time?
     zcash-fetch-params || exit 1
